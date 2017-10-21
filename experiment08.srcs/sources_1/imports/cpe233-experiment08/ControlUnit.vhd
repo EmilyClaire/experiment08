1a8d2fadf2bb7f0578420d397bb63df8df718344
Donald <dhersam@calpoly.edu>
2017-10-20 17:32:46 -0700

    Addc

--- experiment08.srcs/sources_1/imports/cpe233-experiment08/ControlUnit.vhd
+++ experiment08.srcs/sources_1/imports/cpe233-experiment08/ControlUnit.vhd
@@ -172,6 +172,33 @@ begin
   SP_DECR        <= '0';     SCR_ADDR_SEL   <= '0';       FLAG_Z_CLR     <= '0';                 
                                                                         
   IO_STRB        <= '0';     PC_RST            <= '0'; 
+  
+  
+  					-- ADDC R-Imm -------------------
+when "0000100" =>   
+
+PC_LD          <= '0';     RF_WR          <= '1';       FLAG_C_LD      <= '1';     I_SET          <= '0';
+PC_INC         <= '0';     RF_WR_SEL      <= "00";      FLAG_C_SET     <= '0';     I_CLR          <= '0';
+PC_MUX_SEL     <= "00";    ALU_OPY_SEL    <= '1';       FLAG_C_CLR     <= '0';
+           ALU_SEL        <= "0001";                               FLAG_LD_SEL    <= '0';
+SP_LD          <= '0';     FLAG_Z_LD      <= '1';       FLAG_SHAD_LD   <= '0';
+SP_INCR        <= '0';     SCR_WR         <= '0';       FLAG_Z_SET     <= '0';
+SP_DECR        <= '0';     SCR_ADDR_SEL   <= '0';       FLAG_Z_CLR     <= '0';                 
+                                                      
+IO_STRB        <= '0';     PC_RST            <= '0'; 
+  
+  					-- ADDC R-R -------------------
+when "0000100" =>   
+
+PC_LD          <= '0';     RF_WR          <= '1';       FLAG_C_LD      <= '1';     I_SET          <= '0';
+PC_INC         <= '0';     RF_WR_SEL      <= "00";      FLAG_C_SET     <= '0';     I_CLR          <= '0';
+PC_MUX_SEL     <= "00";    ALU_OPY_SEL    <= '0';       FLAG_C_CLR     <= '0';
+           ALU_SEL        <= "0001";                               FLAG_LD_SEL    <= '0';
+SP_LD          <= '0';     FLAG_Z_LD      <= '1';       FLAG_SHAD_LD   <= '0';
+SP_INCR        <= '0';     SCR_WR         <= '0';       FLAG_Z_SET     <= '0';
+SP_DECR        <= '0';     SCR_ADDR_SEL   <= '0';       FLAG_Z_CLR     <= '0';                 
+                                                      
+IO_STRB        <= '0';     PC_RST            <= '0'; 
 		
 						
 					-- BRN -------------------
